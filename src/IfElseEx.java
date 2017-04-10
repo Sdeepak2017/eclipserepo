2f4dbea6dddedd816793194c26c9b5a798bf3ba7
DS00432238 <DS00432238@INBASDPC05936.TechMahindra.com>
2017-04-10 10:52:09 +0530

    First Commit from eclipse

---
+++ src/IfElseEx.java
@@ -0,0 +1,11 @@
+
+public class IfElseEx {
+public static void main(String args[]){
+	int num=877;
+	if (num%2==0){
+		System.out.println("even number");
+	}else{
+			System.out.println("odd");
+	}
+}
+}
---
+++ src/ifelseif.java
@@ -0,0 +1,21 @@
+
+public class ifelseif {
+	public static void main(String args[]){
+		int marks=98;
+	if(marks<35){
+		System.out.println("Fail");
+	}else if(marks>35&&marks<50){
+	System.out.println("D grade");	
+	}else if(marks>50&&marks<65){
+		System.out.println("C grade");
+	}else if(marks>65&&marks<80){
+		System.out.println("B grade");
+	}else if(marks>80&&marks<90){
+		System.out.println("A grade");
+		}else if(marks>90){
+		System.out.println("S grade");
+}else{
+	System.out.println("Invalid!");
+}
+}
+}
